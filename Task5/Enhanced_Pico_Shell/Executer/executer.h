5614d3ffe3e4b02f5cb7c1e83882d4e63fadb16d
Adnan-Adel <adnanadel439@gmail.com>
2024-02-07 20:34:50 +0200

    Adding Task5

---
+++ Task5/Enhanced_Pico_Shell/Executer/executer.h
@@ -0,0 +1,24 @@
+/* --------------------- Includes --------------------- */
+#include"../String/my_string.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+//#include <sys/wait.h>
+#include <unistd.h>
+
+/* --------------------- Macros Declarations --------------------- */
+#define COMMANDS_LENGTH                 5
+#define COMMAND_ARGS_MAX_LENGTH         10
+
+/* --------------------- data types Declarations --------------------- */
+
+/* --------------------- Functions Declarations --------------------- */
+uint8_t is_valid_Command(char* str);
+
+void executer(char** ar, uint32_t length);
+
+void execute_exit(void);
+void execute_cls(void);
+void execute_pwd(void);
+void execute_echo(char** arg, uint32_t length);
+void execute_cd(char** arg, uint32_t length);
